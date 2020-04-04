c8d9bc145c915fabe381f73b9c1c604a049fedde
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2020-04-04 12:16:35 -0500

    trained more

--- inp_dwn_out_img.py
+++ inp_dwn_out_img.py
@@ -21,89 +21,89 @@ class my_cnn(torch.nn.Module):
         return x
 
 # Create the training dataset.
-# training_size = 2000
-# training_x = []
-# training_y = []
-# training_n = []
-# n_level = .01
-# kernel = tools.get_gaussian_kernel(33, 5)
-# for _ in range(training_size):
-#     x = tools.get_stably_bounded_shape(- 3, 3, - 3, 3, 64, 64)
-#     training_x.append(x)
-#     y = signal.fftconvolve(x, kernel, mode='valid')
-#     training_y.append(y)
-#     n = np.random.randn(* y.shape) * n_level
-#     training_n.append(n)
-# training_x = np.array(training_x)
-# training_x = torch.from_numpy(training_x).view(- 1, 1, * training_x[0].shape).float()
-# training_y = np.array(training_y)
-# training_y = torch.from_numpy(training_y).view(- 1, 1, * training_y[0].shape).float()
-# training_n = np.array(training_n)
-# training_n = torch.from_numpy(training_n).view(- 1, 1, * training_n[0].shape).float()
-
-# # Train
-# n_epochs = 1000
-# model = my_cnn()
-# model.load_state_dict(torch.load('model1'))
-# optimizer = torch.optim.AdamW(model.parameters())
-# loss_function = torch.nn.MSELoss()
-# for i in range(n_epochs):
-#     optimizer.zero_grad()
-#     output = model(training_y + training_n)
-#     target = training_x
-#     loss = loss_function(output, target)
-#     loss.backward()
-#     optimizer.step()
-#     print(i, '/', n_epochs, loss.item())
-# torch.save(model.state_dict(), 'model1')
-
-# # Display
-# for i in range(10):
-#     n_input = training_x[i][0]
-#     n_output = model((training_y + training_n)[i].view(1, * (training_y + training_n)[i].shape)).round().view(64, 64).detach()
-#     plt.figure()
-#     plt.imshow(n_input)
-#     plt.gray()
-#     plt.figure()
-#     plt.imshow(n_output)
-#     plt.gray()
-# plt.show()
-
-# Test
-n_level = .05
-kernel = tools.get_gaussian_kernel(33, 5)
-model = my_cnn()
-model.load_state_dict(torch.load('model1'))
-model.eval()
+training_size = 2000
 training_x = []
 training_y = []
 training_n = []
-x = tools.get_stably_bounded_shape(- 3, 3, - 3, 3, 64, 64)
-training_x.append(x)
-y = signal.fftconvolve(x, kernel, mode='valid')
-training_y.append(y)
-n = np.random.randn(* y.shape) * n_level
-training_n.append(n)
+n_level = .01
+kernel = tools.get_gaussian_kernel(33, 5)
+for _ in range(training_size):
+    x = tools.get_stably_bounded_shape(- 3, 3, - 3, 3, 64, 64)
+    training_x.append(x)
+    y = signal.fftconvolve(x, kernel, mode='valid')
+    training_y.append(y)
+    n = np.random.randn(* y.shape) * n_level
+    training_n.append(n)
 training_x = np.array(training_x)
 training_x = torch.from_numpy(training_x).view(- 1, 1, * training_x[0].shape).float()
 training_y = np.array(training_y)
 training_y = torch.from_numpy(training_y).view(- 1, 1, * training_y[0].shape).float()
 training_n = np.array(training_n)
 training_n = torch.from_numpy(training_n).view(- 1, 1, * training_n[0].shape).float()
-n_output = model(training_y + training_n)
-imgin = (training_y + training_n).view(32, 32)
-imgout = n_output.view(64, 64).round().detach()
-imgorig = training_x.view(64, 64)
-plt.figure()
-plt.imshow(imgin)
-plt.gray()
-plt.title('imgin')
-plt.figure()
-plt.imshow(imgorig)
-plt.gray()
-plt.title('imgorig')
-plt.figure()
-plt.imshow(abs(imgout - imgorig))
-plt.gray()
-plt.title('err')
+
+# Train
+n_epochs = 1000
+model = my_cnn()
+model.load_state_dict(torch.load('model1'))
+optimizer = torch.optim.AdamW(model.parameters())
+loss_function = torch.nn.MSELoss()
+for i in range(n_epochs):
+    optimizer.zero_grad()
+    output = model(training_y + training_n)
+    target = training_x
+    loss = loss_function(output, target)
+    loss.backward()
+    optimizer.step()
+    print(i, '/', n_epochs, loss.item())
+torch.save(model.state_dict(), 'model1')
+
+# Display
+for i in range(10):
+    n_input = training_x[i][0]
+    n_output = model((training_y + training_n)[i].view(1, * (training_y + training_n)[i].shape)).round().view(64, 64).detach()
+    plt.figure()
+    plt.imshow(n_input)
+    plt.gray()
+    plt.figure()
+    plt.imshow(n_output)
+    plt.gray()
 plt.show()
+
+# # Test
+# n_level = .05
+# kernel = tools.get_gaussian_kernel(33, 5)
+# model = my_cnn()
+# model.load_state_dict(torch.load('model1'))
+# model.eval()
+# training_x = []
+# training_y = []
+# training_n = []
+# x = tools.get_stably_bounded_shape(- 3, 3, - 3, 3, 64, 64)
+# training_x.append(x)
+# y = signal.fftconvolve(x, kernel, mode='valid')
+# training_y.append(y)
+# n = np.random.randn(* y.shape) * n_level
+# training_n.append(n)
+# training_x = np.array(training_x)
+# training_x = torch.from_numpy(training_x).view(- 1, 1, * training_x[0].shape).float()
+# training_y = np.array(training_y)
+# training_y = torch.from_numpy(training_y).view(- 1, 1, * training_y[0].shape).float()
+# training_n = np.array(training_n)
+# training_n = torch.from_numpy(training_n).view(- 1, 1, * training_n[0].shape).float()
+# n_output = model(training_y + training_n)
+# imgin = (training_y + training_n).view(32, 32)
+# imgout = n_output.view(64, 64).round().detach()
+# imgorig = training_x.view(64, 64)
+# plt.figure()
+# plt.imshow(imgin)
+# plt.gray()
+# plt.title('imgin')
+# plt.figure()
+# plt.imshow(imgorig)
+# plt.gray()
+# plt.title('imgorig')
+# plt.figure()
+# plt.imshow(abs(imgout - imgorig))
+# plt.gray()
+# plt.title('err')
+# plt.show()
